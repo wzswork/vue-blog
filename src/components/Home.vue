16d1e4fdea8b425d19612beb973f6b92c1eb70f4
wangzi <wzswork@hotmail.com>
2018-03-01 16:27:45 +0800

    index

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,93 @@
 <template>
-    <div class="back">
-        <div class="menu">
-            <ul>
-                <li v-for="{{item in links}}" :key="item.index" @click="{{item.route}}">{{item.content}}</li>
-            </ul>
+  <div class="back">
+    <div class="main">
+      <h1 class="head">
+        石头楼
+      </h1>
+      <div class="menu fix">
+        <ul>
+          <li class="active">首页</li>
+          <li>文章</li>
+          <li>小demo</li>
+        </ul>
+      </div>
+      <div class="content fix">
+        <div class="content-left">
+          <digest></digest>
         </div>
+        <div class="content-right">
+          <quick-labels></quick-labels>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
+<style lang="scss" scoped>
+  .back{
+    height: 100%;
+    background: url(../images/home/back-1.png);
+  }
+  .main{
+    margin: 0 auto;
+    width: 1280px;
+  }
+  @media screen and (max-width: 1281px){
+    .main{
+      width: 100%;
+    }
+  }
+  .head{
+    margin: 0;
+    line-height: 100px;
+  }
+  .menu{
+    background: rgba($color: #fff, $alpha: .6);
+    border-radius: 8px;
+    li{
+      position: relative;
+      padding: 7px 18px; 
+      line-height: 32px;
+      float: left;
+      font-size: 18px;
+      color: #666;
+      cursor: pointer;
+      &:hover{
+        color: burlywood;
+      }
+      &.active{
+        color: saddlebrown;
+      }
+    }
+    li+li:before{
+      position: absolute;
+      content: "";
+      left: 0;
+      top: 10px;
+      width: 2px;
+      border-radius: 4px;
+      height: 22px;
+      background-color: #888;
+      opacity: .3;
+    }
+    .content{
+      margin-top: 15px;
+      .content-left{
+        float: left;
+        width: 70%;
+        height: 100%;
+        background: rgba($color: #fff, $alpha: .7)
+      }
+      .content-right{
+        float: right;
+        width: 30%;
+      }
+      @media screen and (max-width: 240px) {
+        .content-left, .content-right{
+          float: none;
+          width: 100%;
+        }
+      }
+    }
+  }
+</style>
+
