81aa44d4dc6378189ec2e5c21ec5a97e2a481bec
wangzi <wzswork@hotmail.com>
2018-03-04 11:02:04 +0800

    12080304

--- src/components/Digest.vue
+++ src/components/Digest.vue
@@ -1,3 +1,33 @@
 <template>
-  
+  <ul>
+      <li></li>
+  </ul>
 </template>
+<style lang="scss" scoped>
+
+</style>
+<script>
+  import mapState from 'vuex'
+  export default {
+    data: {
+
+    },
+    computed: {
+      ...mapState({
+        curPage:state=>state.curPage,
+        curLabel:state=>state.curLabel
+      }),
+      textData(){
+        _this = this;
+        this.axios.get('/api/digests', {
+          params: {
+            label:_this.curLabel,
+            page:_this.curPage
+          }
+        }).then(function(res){
+          console.log(res);
+        })
+      }
+    }
+  }
+</script>
