088ccc754ddaa1b8022abe103ead3c3a50aefee7
王阿石 <wzswork@hotmail.com>
2018-03-20 19:32:49 +0800

    li

--- src/components/ComboSelector.vue
+++ src/components/ComboSelector.vue
@@ -2,7 +2,12 @@
   <div class="combo">
     <input type="text">
     <ul>
-      
+      <li v-for="item in lists" :key="item.id">{{item.name}}</li>
     </ul>
   </div>
 </template>
+<script>
+  export default {
+     
+  }
+</script>
